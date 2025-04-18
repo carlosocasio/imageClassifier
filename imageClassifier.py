13611fe70ae0680908d9a52542e3d199638894e0
Carlos Ocasio <carlos@ocasio.me>
2025-04-17 23:23:00 -0400

    Update imageClassifier.py

--- imageClassifier.py
+++ imageClassifier.py
@@ -29,7 +29,7 @@ app_mode = st.sidebar.selectbox(':blue[Select Page]',['Home','Upload and Predict
 css="""
 <style>
     [data-testid="stSidebar"] {
-        background: #eba834;
+        background: #1d1c8a;
 		# color: blue;
     }
 </style>
