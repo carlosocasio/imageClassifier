a0a71fdecedb043946c311c6d7604bd37c7d2441
Carlos Ocasio <carlos@ocasio.me>
2025-04-17 22:43:31 -0400

    Update imageClassifier.py

--- imageClassifier.py
+++ imageClassifier.py
@@ -44,7 +44,7 @@ path = path+'/ResNet50V2-AIvsHumanGenImages.keras'
 model = load_model(path)
 
 if app_mode=='Home':   
-    st.title('Artificial Intelligence versus Human Intelligence')
+    st.title('AI versus Human Intelligence')
     st.write("Upload images to determine if they are AI or Human generated images")
     st.image('ai-human.jpg')    
 
