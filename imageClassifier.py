98b22e738b4a8488cb9d96f39b1c26315950da24
Carlos Ocasio <carlos@ocasio.me>
2025-11-05 10:04:40 -0500

    Update imageClassifier.py

--- imageClassifier.py
+++ imageClassifier.py
@@ -35,12 +35,15 @@ css="""
 st.write(css, unsafe_allow_html=True)
 
 
+
 # Download latest version
-path = kagglehub.model_download("utkarshsaxenadn/ai-vs-human/tensorFlow2/default")
+# path = kagglehub.model_download("utkarshsaxenadn/ai-vs-human/tensorFlow2/default")
+path = kagglehub.model_download("utkarshsaxenadn/ai-vs-human/tensorflow2/default/1/ResNet50V2-AIvsHumanGenImages.keras")
+
 print("Path to downloaded model:", path)
 
 # Load the trained model (Use the single-input classifier version) :
-path = path + "/AIvsHuman_ResNet50V2.h5"
+# path = path + "/AIvsHuman_ResNet50V2.h5"
 model = load_model(path, compile=False)
 
 if app_mode=='Home':   
